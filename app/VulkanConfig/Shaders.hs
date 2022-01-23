9c26d741eec948f3ccfd4e88f2d36140cb5e6b4d
Jakob Bruenker <jakob.bruenker@gmail.com>
2022-01-23 09:03:59 +0100

    fixed vertex buffer issue

--- app/VulkanConfig/Shaders.hs
+++ app/VulkanConfig/Shaders.hs
@@ -20,33 +20,29 @@ type VertexData =
 type VertexDefs =
   '[ "position"  ':-> Input      '[Location 0] (V 2 Float)
    , "color"     ':-> Input      '[Location 1] (V 3 Float)
-   , "pointSize" ':-> Output     '[Location 0] Float
-   , "vertColor" ':-> Output     '[Location 1] (V 4 Float)
+   , "vertColor" ':-> Output     '[Location 0] (V 4 Float)
    , "main"      ':-> EntryPoint '[          ] Vertex
    ]
 
 vertex :: ShaderModule "main" VertexShader VertexDefs _
 vertex = shader do
   position <- #position
-  #gl_Position .= Vec4 (view @(Swizzle "x") position) (view @(Swizzle "y") position) 0 0
+  #gl_Position .= Vec4 (view @(Swizzle "x") position) (view @(Swizzle "y") position) 0 1
   color <- #color
   #vertColor .=
     Vec4 (view @(Swizzle "x") color) (view @(Swizzle "y") color) (view @(Swizzle "z") color) 1
-  #pointSize .= 40
   #gl_PointSize .= 40
 
 type FragmentDefs =
-  '[ "pointSize" ':-> Input      '[Location 0     ] Float
-   , "vertColor" ':-> Input      '[Location 1     ] (V 4 Float)
+  '[ "vertColor" ':-> Input      '[Location 0     ] (V 4 Float)
    , "color"     ':-> Output     '[Location 0     ] (V 4 Float)
    , "main"      ':-> EntryPoint '[OriginUpperLeft] Fragment
    ]
 
 fragment :: ShaderModule "main" FragmentShader FragmentDefs _
 fragment = shader do
-  pointSize <- #pointSize
   pCoord <- #gl_PointCoord
-  let col = Vec4 (1 - pointSize / 50) (pointSize / 50) 0.5 1
+  col <- #vertColor
 
   -- Limit alpha to a disk with darkened limb
   #color .= set @(Index 3) (1 - squaredNorm (pCoord ^* 2 ^-^ Vec2 1 1)) col
