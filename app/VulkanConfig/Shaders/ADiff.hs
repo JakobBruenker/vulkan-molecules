142c5890cc33cbfff172444bf460168a883eb055
Jakob Brünker <10101851+JakobBruenker@users.noreply.github.com>
2024-01-05 07:26:39 +0100

    ADiff parser

--- app/VulkanConfig/Shaders/ADiff.hs
+++ app/VulkanConfig/Shaders/ADiff.hs
@@ -2,18 +2,22 @@
 
 module VulkanConfig.Shaders.ADiff where
 
-import Prelude
+import RIO
+import RIO.List (iterate)
+import RIO.List.Partial (tail, foldr1)
+import RIO.NonEmpty qualified as NE
+
 import Data.List (find, intercalate, sort)
 import FIR qualified
-import Data.String (IsString)
-import Data.Maybe (fromJust, fromMaybe)
-import Data.List.NonEmpty (nonEmpty, NonEmpty ((:|)), groupWith)
-import Control.Applicative (Applicative(liftA2))
-import Data.Foldable (toList)
+import Data.Maybe (fromJust)
 import Data.Tuple.Extra (fst3, thd3, snd3)
+import Text.Parsec qualified as P
+import Text.Parsec.Expr
+import Text.Parsec.Token qualified as P
+import Text.Parsec.Language (haskellDef)
 
 data Expr a where
-  (:+), (:*), (:**) :: Expr a -> Expr a -> Expr a
+  (:+), (:*), (:^) :: Expr a -> Expr a -> Expr a
   Ln :: Expr a -> Expr a
   C :: a -> Expr a
   Var :: String -> Expr a
@@ -25,19 +29,19 @@ pattern e1 :- e2 <- e1 :+ C ((== negOne) -> True) :* e2
     e1 :- e2 = e1 :+ C negOne :* e2
 
 pattern (:/) :: (Eq a, DiffInt a) => Expr a -> Expr a -> Expr a
-pattern e1 :/ e2 <- e1 :* e2 :** C ((== negOne) -> True)
+pattern e1 :/ e2 <- e1 :* e2 :^ C ((== negOne) -> True)
   where
-    e1 :/ e2 = e1 :* e2 :** C negOne
+    e1 :/ e2 = e1 :* e2 :^ C negOne
 
 infixl 6 :+, :-
 infixl 7 :*, :/
-infixr 8 :**
+infixr 8 :^
 
 toMonomial :: DiffInt a => Expr a -> Maybe (a, String, Expr a)
 toMonomial = \case
   C a :* Var v -> Just (a, v, C one)
-  C a :* Var v :** b -> Just (a, v, b)
-  Var v :** b -> Just (one, v, b)
+  C a :* Var v :^ b -> Just (a, v, b)
+  Var v :^ b -> Just (one, v, b)
   Var v -> Just (one, v, C one)
   _ -> Nothing
 
@@ -47,9 +51,9 @@ pattern Monomial e <- (toMonomial -> Just e)
     Monomial = \case
       (Zero, _, _    ) -> C zero
       (One , v, C One) ->        Var v
-      (One , v, b    ) ->        Var v :** b
+      (One , v, b    ) ->        Var v :^ b
       (a   , v, C One) -> C a :* Var v
-      (a   , v, b    ) -> C a :* Var v :** b
+      (a   , v, b    ) -> C a :* Var v :^ b
 
 toSum :: Expr a -> Maybe [Expr a]
 toSum = \case
@@ -59,7 +63,7 @@ toSum = \case
 pattern Sum :: DiffInt a => [Expr a] -> Expr a
 pattern Sum es <- (toSum -> Just es)
   where
-    Sum = maybe (C zero) (foldr1 (:+)) . nonEmpty
+    Sum = maybe (C zero) (foldr1 (:+)) . NE.nonEmpty
 
 toProduct :: Expr a -> Maybe [Expr a]
 toProduct = \case
@@ -69,10 +73,10 @@ toProduct = \case
 pattern Product :: DiffInt a => [Expr a] -> Expr a
 pattern Product es <- (toProduct -> Just es)
   where
-    Product = maybe (C one) (foldr1 (:*)) . nonEmpty
+    Product = maybe (C one) (foldr1 (:*)) . NE.nonEmpty
 
-{-# COMPLETE C, Var, (:**), Ln, Sum, Product #-}
-{-# COMPLETE C, Monomial, (:**), Ln, Sum, Product #-}
+{-# COMPLETE C, Var, (:^), Ln, Sum, Product #-}
+{-# COMPLETE C, Monomial, (:^), Ln, Sum, Product #-}
 
 instance (Ord a, DiffInt a) => Ord (Expr a) where
   compare = \cases
@@ -86,11 +90,11 @@ instance (Ord a, DiffInt a) => Ord (Expr a) where
         GT -> GT
       GT -> GT
     (Monomial _) _ -> LT
-    (e1 :** e2) (e3 :** e4) -> case compare e1 e3 of
+    (e1 :^ e2) (e3 :^ e4) -> case compare e1 e3 of
       LT -> LT
       EQ -> e2 `compare` e4
       GT -> GT
-    (_ :** _) _ -> LT
+    (_ :^ _) _ -> LT
     (Ln e1) (Ln e2) -> e1 `compare` e2
     (Ln _) _ -> LT
     (Sum es1) (Sum es2) -> case compare (length es1) (length es2) of
@@ -104,13 +108,13 @@ instance (Ord a, DiffInt a) => Ord (Expr a) where
       GT -> GT
     (Product _) _ -> LT
 
-instance (Num a, Show a) => Show (Expr a) where
+instance (DiffInt a, Show a) => Show (Expr a) where
   show = \case
     C a -> show a
     Var v -> v
     Sum es -> "(" ++ intercalate " + " (show <$> es) ++ ")"
     Product es -> "(" ++ intercalate " * " (show <$> es) ++ ")"
-    e1 :** e2 -> "(" ++ show e1 ++ " ** " ++ show e2 ++ ")"
+    e1 :^ e2 -> "(" ++ show e1 ++ " ^ " ++ show e2 ++ ")"
     Ln e -> "ln " ++ show e
 
 instance IsString (Expr a) where
@@ -157,7 +161,7 @@ diff v = \case
   Var v' -> C $ if v == v' then one else zero
   e1 :+ e2 -> diff v e1 :+ diff v e2
   e1 :* e2 -> e1 :* diff v e2 :+ diff v e1 :* e2
-  e1 :** e2 -> e1 :** e2 :* (diff v e2 :* Ln e1 :+ e2 :* diff v e1 :/ e1)
+  e1 :^ e2 -> e1 :^ e2 :* (diff v e2 :* Ln e1 :+ e2 :* diff v e1 :/ e1)
   Ln e -> diff v e :/ e
 
 partitionExprs :: (DiffInt a, Eq a) => [Expr a] -> ([a], [(a, String, Expr a)], [Expr a])
@@ -180,19 +184,49 @@ simplify = fst . fromJust . find (uncurry (==)) . (zip <*> tail) . iterate step
       Sum es -> Sum $ concat [[C c' | c' /= zero], ms', sort $ map step es']
         where
           (cs, ms, es') = partitionExprs es
-          ms' = map (\mons@((_, v, p) :| _) -> Monomial (sum $ fmap fst3 mons, v, step p)) . groupWith (\(_, v, p) -> (v, p)) . sort $ ms
+          ms' = map (\mons@((_, v, p) :| _) -> Monomial (sum $ fmap fst3 mons, v, step p)) . NE.groupWith (\(_, v, p) -> (v, p)) . sort $ ms
           c' = sum cs
 
       Product es -> if c' == zero then C zero else Product $ concat [[C c' | c' /= one], ms', sort $ map step es']
         where
           (cs, ms, es') = partitionExprs es
-          ms' = map (\mons@((_, v, _) :| _) -> Monomial (product $ fmap fst3 mons, v, Sum . sort . toList $ fmap (step . thd3) mons)) . groupWith snd3 . sort $ ms
+          ms' = map (\mons@((_, v, _) :| _) -> Monomial (product $ fmap fst3 mons, v, Sum . sort . toList $ fmap (step . thd3) mons)) . NE.groupWith snd3 . sort $ ms
           c' = product cs
 
-      (a :** b) :** c -> a :** (b :* c)
-      a :** b -> step a :** step b
+      (a :^ b) :^ c -> step a :^ (step b :* step c)
+      _ :^ C Zero -> C one
+      a :^ C One -> step a
+      a :^ b -> step a :^ step b
 
+      Ln (C One) -> C zero
+      Ln (a :^ b) -> step b :* Ln (step a)
       Ln a -> Ln (step a)
+
+parse :: (Eq a, Fractional a, DiffInt a) => String -> Either P.ParseError (Expr a)
+parse = P.parse expr ""
+  where
+    expr = buildExpressionParser table term
+    term = parens expr <|> C <$> number <|> Var <$> identifier
+    table =
+      [ [prefix "ln" Ln]
+      , [binary "^" (:^) AssocRight]
+      , [binary "*" (:*) AssocLeft, binary "/" (:/) AssocLeft]
+      , [binary "+" (:+) AssocLeft, binary "-" (:-) AssocLeft, prefix "-" (C -1 :*), prefix "+" id]
+      ]
+
+    
+    binary  name fun assoc = flip Infix assoc do {reservedOp name; return fun}
+    prefix  name fun       = Prefix           do {reservedOp name; return fun}
+
+    lexer = P.makeTokenParser haskellDef
+      { P.reservedOpNames = ["+", "-", "*", "/", "^"]
+      , P.reservedNames = ["ln"]
+      }
+
+    parens = P.parens lexer
+    number = either fromIntegral realToFrac <$> P.naturalOrFloat lexer
+    identifier = P.identifier lexer
+    reservedOp = P.reservedOp lexer
 
 -- TODO:
 -- - interface with TH
